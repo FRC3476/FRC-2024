984097ecea9aa41a7c465f8b82d3d31c25443ad0
amb2127 <39502367+amb2127@users.noreply.github.com>
2024-01-24 14:22:34 -0800

    chore: cleanup certain parts of the code

--- src/main/java/frc/subsystem/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/subsystem/drive/ModuleIOTalonFX.java
@@ -173,25 +173,12 @@ public class ModuleIOTalonFX implements ModuleIO {
     }
 
     private boolean isBraking = false;
-    private final MotorOutputConfigs coastModeInverted = new MotorOutputConfigs();
-    private final MotorOutputConfigs brakeModeInverted = new MotorOutputConfigs();
-    private final MotorOutputConfigs coastMode = new MotorOutputConfigs();
-    private final MotorOutputConfigs brakeMode = new MotorOutputConfigs();
-    {
-        coastMode.NeutralMode = NeutralModeValue.Coast;
-        brakeMode.NeutralMode = NeutralModeValue.Brake;
 
-        coastModeInverted.NeutralMode = NeutralModeValue.Coast;
-        brakeModeInverted.NeutralMode = NeutralModeValue.Brake;
-
-        coastModeInverted.Inverted = InvertedValue.Clockwise_Positive;
-        brakeModeInverted.Inverted = InvertedValue.Clockwise_Positive;
-    }
     @Override
     public void setBrakeMode(boolean enabled) {
         if (isBraking != enabled) {
-            steerMotor.getConfigurator().apply(enabled ? brakeModeInverted : coastModeInverted);
-            driveMotor.getConfigurator().apply(enabled ? brakeMode : coastMode);
+            steerMotor.setNeutralMode(enabled ? NeutralModeValue.Brake : NeutralModeValue.Coast);
+            driveMotor.setNeutralMode(enabled ? NeutralModeValue.Brake : NeutralModeValue.Coast);
         }
         isBraking = enabled;
     }
@@ -200,8 +187,7 @@ public class ModuleIOTalonFX implements ModuleIO {
 
     @Override
     public void setSteerMotorPosition(double position) {
-        positionVoltage.Position = position/360;
-        steerMotor.setControl(positionVoltage);
+        setSteerMotorPosition(position, 0);
     }
 
     @Override
