4f544eba31d78d20c984b6996faffb61632e87a7
amb2127 <39502367+amb2127@users.noreply.github.com>
2024-02-26 10:21:50 -0800

    delete buffered reader

--- src/main/java/org/codeorange/frc2024/subsystem/Superstructure.java
+++ src/main/java/org/codeorange/frc2024/subsystem/Superstructure.java
@@ -306,82 +306,11 @@ public class Superstructure extends AbstractSubsystem {
         return superstructure;
     }
 
-//    private String path = Filesystem.getDeployDirectory().getPath(); // need to append exact location of CSV file
-//    HashMap<String, ShooterConfiguration> map = new HashMap<String, ShooterConfiguration>();
-//
-//    public void toHashMap() {
-//
-//
-//        String line = "";
-//        String splitBy = ",";
-//        try {
-//            BufferedReader br = new BufferedReader(new FileReader(path));
-//            br.readLine();
-//            while ((line = br.readLine()) != null) {
-//                String[] fields = line.split(splitBy);
-//
-//                String distanceDirectionHeight = fields[0];
-//                String location = fields[1];
-//                double shooterAngle = Double.parseDouble(fields[2]);
-//                double shooterVelocity = Double.parseDouble(fields[3]);
-//
-//                ShooterConfiguration shooterConfiguration = new ShooterConfiguration(location, shooterAngle, shooterVelocity);
-//                map.put(distanceDirectionHeight, shooterConfiguration);
-//
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
-
-    private String convertToKey(double distance, String direction, String height) {
-        return distance + direction + height;
-
-    }
-
-//    public String getLocationOfRobot(double distance, String direction, String height){
-//        return map.get(convertToKey(distance, direction, height)).getLocation();
-//    }
-//    public double getShooterAngle(double distance, String direction, String height){
-//        return map.get(convertToKey(distance, direction, height)).getShooterVelocity();
-//    }
-//    public double getShooterVelocity(double distance, String direction, String height){
-//        return map.get(convertToKey(distance, direction, height)).getShooterAngle();
-//    }
-
     public double getLowShooterAngle(){
         if (drive.findAngleToSpeaker() > Math.PI / 2) {
-            return (Constants.AngleLookupInterpolation.SHOOTER_ANGLE_LOW_FRONT.get(drive.findDistanceToSpeaker()));
+            return (AngleLookupInterpolation.SHOOTER_ANGLE_LOW_FRONT.get(drive.findDistanceToSpeaker()));
         } else {
-            return Constants.AngleLookupInterpolation.SHOOTER_ANGLE_LOW_BACK.get(drive.findDistanceToSpeaker());
-        }
-
-    }
-
-
-    static class ShooterConfiguration {
-        private String location;
-        private double shooterAngle;
-        private double shooterVelocity;
-
-        public ShooterConfiguration(String location, double shooterAngle, double shooterVelocity) {
-            this.location = location;
-            this.shooterAngle = shooterAngle;
-            this.shooterVelocity = shooterVelocity;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public double getShooterAngle() {
-            return shooterAngle;
-        }
-
-        public double getShooterVelocity() {
-            return shooterVelocity;
+            return AngleLookupInterpolation.SHOOTER_ANGLE_LOW_BACK.get(drive.findDistanceToSpeaker());
         }
 
     }
