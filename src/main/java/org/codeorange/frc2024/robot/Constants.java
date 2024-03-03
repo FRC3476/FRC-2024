ffe9c023630b0ce001ae765a5a09433a42138d7b
amb2127 <39502367+amb2127@users.noreply.github.com>
2024-03-02 16:34:24 -0800

    stuff

--- src/main/java/org/codeorange/frc2024/robot/Constants.java
+++ src/main/java/org/codeorange/frc2024/robot/Constants.java
@@ -222,7 +222,7 @@ public final class Constants {
     public static final double SS_HOMING_WRIST = isPrototype() ? 0 : 0;
     public static final double SS_HOMING_CLIMBER = isPrototype() ? 0 : 0;
 
-    public static final double SWERVE_DRIVE_P = 100;
+    public static final double SWERVE_DRIVE_P = 50;
     public static final double SWERVE_DRIVE_D = 0;
     public static final double SWERVE_DRIVE_I = 0;
 
