20c5783bdba1a51a78effbe1d38aedce0a2c2b95
amber <39502367+amb2127@users.noreply.github.com>
2024-03-22 19:43:41 -0700

    Swap vision to advantageKit because it wasn't using it before
    * swap to akit
    
    * delete autobuilder maven from gradle
    
    * my hands r typing words

--- src/main/java/org/codeorange/frc2024/subsystem/vision/Vision.java
+++ src/main/java/org/codeorange/frc2024/subsystem/vision/Vision.java
@@ -1,14 +1,30 @@
 package org.codeorange.frc2024.subsystem.vision;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import org.codeorange.frc2024.robot.Robot;
 import org.codeorange.frc2024.subsystem.AbstractSubsystem;
+import org.codeorange.frc2024.subsystem.drive.Drive;
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
-public class Vision extends AbstractSubsystem {
-    public static final String LL_FRONT = "limelight-front";
-    public static final String LL_BACK = "limelight-back";
+import java.util.Arrays;
 
-    public final Limelight frontCamera;
-    public final Limelight backCamera;
+import static org.codeorange.frc2024.robot.Constants.FIELD_LENGTH_METERS;
+import static org.codeorange.frc2024.robot.Constants.FIELD_WIDTH_METERS;
+
+public class Vision extends AbstractSubsystem {
+    private final VisionIO[] visionIO;
+    private final VisionInputsAutoLogged[] visionInputs;
+    private static final String LL_FRONT = "limelight-front";
+    private static final String LL_BACK = "limelight-back";
+    private static Drive drive;
+    double fieldBorderMargin = 0.15;
+    double defaultXYStdev = 0.7;
+
+
     public static final LoggedDashboardChooser<Boolean> visionChooser;
     public static final LoggedDashboardChooser<Boolean> unconditionallyTrustVision;
     static {
@@ -20,20 +36,66 @@ public class Vision extends AbstractSubsystem {
         unconditionallyTrustVision.addOption("on", true);
     }
 
-    public Vision() {
-        super();
-        frontCamera = new Limelight(LL_FRONT);
-        backCamera = new Limelight(LL_BACK);
+    public Vision(VisionIO... cameras) {
+        visionIO = cameras;
+        visionInputs = new VisionInputsAutoLogged[cameras.length];
+        Arrays.fill(visionInputs, new VisionInputsAutoLogged());
+        drive = Robot.getDrive();
     }
 
     @Override
     public synchronized void update() {
-        frontCamera.update();
-        backCamera.update();
+        for(int i = 0; i < visionIO.length; i++) {
+            visionIO[i].updateInputs(visionInputs[i]);
+            Logger.processInputs("Vision/" + visionIO[i].getName(), visionInputs[i]);
+
+            processVisionData(visionIO[i], visionInputs[i]);
+        }
     }
 
-    public void setVisionForAuto(boolean enabled) {
-        frontCamera.setVisionForAuto(enabled);
-        backCamera.setVisionForAuto(enabled);
+    private void processVisionData(VisionIO io, VisionIO.VisionInputs inputs) {
+        if(unconditionallyTrustVision.get()) {
+            drive.addVisionMeasurement(
+                    inputs.botPose2d,
+                    inputs.timestamp,
+                    VecBuilder.fill(0.01, 0.01, 1)
+            );
+        }
+
+        //exit if data sucks
+        if(inputs.botPose2d.equals(new Pose2d()) || inputs.botPose3d.equals(new Pose3d())) {
+            return;
+        }
+
+        //exit if off the field, or too far above or below the ground
+        if(
+                inputs.botPose3d.getX() < -fieldBorderMargin
+                        || inputs.botPose3d.getX() > fieldBorderMargin + FIELD_LENGTH_METERS
+                        || inputs.botPose3d.getY() < -fieldBorderMargin
+                        || inputs.botPose3d.getY() > fieldBorderMargin + FIELD_WIDTH_METERS
+                        || inputs.botPose3d.getZ() < -0.4
+                        || inputs.botPose3d.getZ() > 0.1
+        ) {
+            return;
+        }
+
+        //exit if rotation doesn't match gyro measurement
+        if(Math.abs(drive.getPose().getRotation().minus(inputs.botPose2d.getRotation()).getDegrees()) > 5) {
+            return;
+        }
+
+        //exit if tags are too far in auto
+        if(inputs.avgDist > 4 && DriverStation.isAutonomous()) {
+            return;
+        }
+
+        Logger.recordOutput("Vision/" + io.getName() + "/Accepted Pose", inputs.botPose2d);
+
+        //scale stdevs with square of distance and tag count
+        double xyStdev = defaultXYStdev * inputs.avgDist * inputs.avgDist / inputs.tagCount / inputs.tagCount;
+
+        Logger.recordOutput("Vision/" + io.getName() + "/XY Standard Deviations", xyStdev);
+        io.getDashboardField().setRobotPose(inputs.botPose2d);
+        drive.addVisionMeasurement(inputs.botPose2d, inputs.timestamp, VecBuilder.fill(xyStdev, xyStdev, 9999999));
     }
 }
